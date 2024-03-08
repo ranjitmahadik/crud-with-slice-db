8c3e5b3e42fc3f19bf7913bbcb33edf8c6d308a5
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-08 20:19:02 +0530

    - add delay and retries.

--- taskqueue/tasks.py
+++ taskqueue/tasks.py
@@ -6,19 +6,20 @@ import logging
 redis: IKVRepository = RedisRepositoryImpl()
 logger = logging.getLogger("worker")
 
-@huey.task()
+
+@huey.task(retries=10, retry_delay=1000)
 def create_post(post):
     logger.info(f"create post called with {post}")
     return redis.create_post(post)
 
 
-@huey.task()
+@huey.task(retries=10, retry_delay=1000)
 def delete_post(key: str):
     logger.info(f"delete_post called with {key}")
     return redis.delete_post(key)
 
 
-@huey.task()
+@huey.task(retries=10, retry_delay=1000)
 def add_post_if_not_exists(post):
     logger.info("add_post_if_not_exists called with {post}")
     return redis.add_post_if_not_exists(post)
